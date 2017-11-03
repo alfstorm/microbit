6834a6210d2a56cfd5f01cc3ddcaff2b93e6f123
Alf Storm <alf.storm@gmail.com>
2017-11-03 21:30:24 +0100

    Imperial March with synchronized LEDs.

---
+++ javascript/imperial_march.js
@@ -0,0 +1,155 @@
+input.onButtonPressed(Button.A, () => {
+    music.setTempo(35)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `, 0)
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `, 0)
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(311, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(466, music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(311, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        . . . . .
+        `, 0)
+    music.playTone(466, music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # # # # #
+        `, 0)
+    music.playTone(392, music.beat(BeatFraction.Half))
+})
+
+input.onButtonPressed(Button.B, () => {
+    music.setTempo(35)
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `, 0)
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `, 0)
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `, 0)
+    music.playTone(622, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `, 0)
+    music.playTone(466, music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `, 0)
+    music.playTone(370, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `, 0)
+    music.playTone(311, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `, 0)
+    music.playTone(466, music.beat(BeatFraction.Sixteenth))
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `, 0)
+    music.playTone(392, music.beat(BeatFraction.Half))
+})

775c005b5ecbb96467161c2a1ab931d956d2591c
Alf Storm <alf.storm@gmail.com>
2017-11-03 21:47:28 +0100

    Simple program playing scales.

---
+++ javascript/scale_up_down.js
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, () => {
+    led_play(0, 4, 523, BeatFraction.Half)
+    led_play(0, 3, 587, BeatFraction.Half)
+    led_play(0, 2, 659, BeatFraction.Half)
+    led_play(0, 1, 698, BeatFraction.Half)
+    led_play(0, 0, 784, BeatFraction.Whole)
+})
+
+input.onButtonPressed(Button.B, () => {
+    led_play(4, 0, 784, BeatFraction.Half)
+    led_play(4, 1, 698, BeatFraction.Half)
+    led_play(4, 2, 659, BeatFraction.Half)
+    led_play(4, 3, 587, BeatFraction.Half)
+    led_play(4, 4, 523, BeatFraction.Whole)
+})
+
+function led_play(x: number, y: number, tone: number, duration: BeatFraction) {
+    led.plot(x, y)
+    music.playTone(tone, music.beat(duration))
+    led.unplot(x, y)
+}
